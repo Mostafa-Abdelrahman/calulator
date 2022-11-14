e8cee45c60f0d6be370955248074551f68bc42d4
Mostafa Abdelrhman <93885854+Mostafa-Abdelrahman@users.noreply.github.com>
2022-11-14 17:08:22 +0200

    Add files via upload

---
+++ calculator_moustafaaaa/Form1.cs
@@ -0,0 +1,249 @@
+namespace calculator_moustafaaaa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void reminder_Click(object sender, EventArgs e)
+        {
+            sign.Text = "%";
+        }
+
+        private void seven_Click(object sender, EventArgs e)
+        {
+            if (sign.Text == "")
+            {
+                input_one.Text += '7';
+            }
+            else
+            {
+                input_two.Text += '7';
+            }
+        }
+
+        private void minas_Click(object sender, EventArgs e)
+        {
+            sign.Text = "-";
+        }
+
+        private void mult_Click(object sender, EventArgs e)
+        {
+            sign.Text = "*";
+        }
+
+        private void dot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void one_Click(object sender, EventArgs e)
+        {
+            if (sign.Text == "")
+            {
+                input_one.Text += '1';
+            }
+            else
+            {
+                input_two.Text += '1';
+            }
+        }
+
+        private void nine_Click(object sender, EventArgs e)
+        {
+            if (sign.Text == "")
+            {
+                input_one.Text += '9';
+            }
+            else
+            {
+                input_two.Text += '9';
+            }
+        }
+
+        private void four_Click(object sender, EventArgs e)
+        {
+            if (sign.Text == "")
+            {
+                input_one.Text += '4';
+            }
+            else
+            {
+                input_two.Text += '4';
+            }
+        }
+
+        private void Equal_Click(object sender, EventArgs e)
+        {
+            double num1, num2;
+
+            switch (sign.Text)
+            {
+                case "+":
+                     num1 = Convert.ToDouble(input_one.Text);
+                     num2 = Convert.ToDouble(input_two.Text);
+                    result.Text = (num1 + num1).ToString();
+                    break;
+                case "*":
+                     num1 = Convert.ToDouble(input_one.Text);
+                     num2 = Convert.ToDouble(input_two.Text);
+                    result.Text = (num1 * num1).ToString();
+                    break;
+                case "-":
+                    num1 = Convert.ToDouble(input_one.Text);
+                    num2 = Convert.ToDouble(input_two.Text);
+                    result.Text = (num1 - num1).ToString();
+                    break;
+                case "/":
+                    num1 = Convert.ToDouble(input_one.Text);
+                    num2 = Convert.ToDouble(input_two.Text);
+                    result.Text = (num1 / num1).ToString();
+                    break;
+                case "%":
+                    num1 = Convert.ToDouble(input_one.Text);
+                    num2 = Convert.ToDouble(input_two.Text);
+                    result.Text = (num1 % num1).ToString();
+                    break;
+                default:
+                    result.Text = "error";
+                    break;
+                
+            }
+            input_one.Text = "";
+            input_two.Text = "";
+            sign.Text = "";
+        }
+
+        private void zero_Click(object sender, EventArgs e)
+        {
+            if (sign.Text == "")
+            {
+                input_one.Text += '0';
+            }
+            else
+            {
+                input_two.Text += '0';
+            }
+        }
+
+        private void three_Click(object sender, EventArgs e)
+        {
+            if (sign.Text == "")
+            {
+                input_one.Text += '3';
+            }
+            else
+            {
+                input_two.Text += '3';
+            }
+        }
+
+        private void two_Click(object sender, EventArgs e)
+        {
+            if (sign.Text == "")
+            {
+                input_one.Text += '2';
+            }
+            else
+            {
+                input_two.Text += '2';
+            }
+        }
+
+        private void six_Click(object sender, EventArgs e)
+        {
+            if (sign.Text == "")
+            {
+                input_one.Text += '6';
+            }
+            else
+            {
+                input_two.Text += '6';
+            }
+        }
+
+        private void five_Click(object sender, EventArgs e)
+        {
+            if (sign.Text == "")
+            {
+                input_one.Text += '5';
+            }
+            else
+            {
+                input_two.Text += '5';
+            }
+        }
+
+        private void eight_Click(object sender, EventArgs e)
+        {
+            if (sign.Text == "")
+            {
+                input_one.Text += '8';
+            }
+            else
+            {
+                input_two.Text += '8';
+            }
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            sign.Text = "+";
+        }
+
+        private void input_one_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void input_two_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void result_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void on_Click(object sender, EventArgs e)
+        {
+            sign.Text =" ";
+        }
+
+        private void divid_Click(object sender, EventArgs e)
+        {
+            sign.Text = "/";
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            input_two.Text = "";
+            input_one.Text = "";
+            sign.Text = "";
+        }
+
+        private void sign_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void input_box_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
